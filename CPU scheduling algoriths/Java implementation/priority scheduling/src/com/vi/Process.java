48d9b7d5e6cf83afb26ecf13c6294459b7cc0e31
victor <touchwithvictor@gmail.com>
2020-08-19 22:03:49 +0530

    ps

--- CPU scheduling algoriths/Java implementation/priority scheduling/src/com/vi/Process.java
+++ CPU scheduling algoriths/Java implementation/priority scheduling/src/com/vi/Process.java
@@ -1,4 +1,24 @@
 package com.vi;
 
 public class Process {
+    private String processId;
+    private int burstTime;
+    private final int priority;
+    public Process(String processId, int burstTime, int priority) {
+        this.processId = processId;
+        this.burstTime = burstTime;
+        this.priority = priority;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
