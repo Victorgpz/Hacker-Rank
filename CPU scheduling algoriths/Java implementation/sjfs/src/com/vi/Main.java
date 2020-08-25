379bfba902382b21d6d4a2e0399a9f8933439126
Victor <35284183+Victorgpz@users.noreply.github.com>
2020-08-25 23:38:59 +0530

    Update Main.java

--- CPU scheduling algoriths/Java implementation/sjfs/src/com/vi/Main.java
+++ CPU scheduling algoriths/Java implementation/sjfs/src/com/vi/Main.java
@@ -1,71 +1,54 @@
+
 package com.vi;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Random rn=new Random();
         List<Process> processList=new ArrayList<>();
-        for (int i=0;i<11;i++){
-            processList.add(new Process("p"+i,rn.nextInt(10)));
+        for (int i=1;i<10;i++){
+            processList.add(new Process(i,i*2));
         }
 
-        Collections.sort(processList, new Comparator<Process>() {
-            @Override
-            public int compare(Process o1, Process o2) {
-               if(o1.getBurstTime()<o2.getBurstTime()){
-                   return -1;
-               }else if(o1.getBurstTime()>o2.getBurstTime()){
-                   return 1;
-               }else {
-                   return 0;
-               }
-            }
-        });
         int[] waitingTime=waitingTime(processList);
         int[] turnAroundTime=turnAroundTime(processList,waitingTime);
         printList(processList,waitingTime,turnAroundTime);
 
     }
-    public static int[] waitingTime(List<? extends Process> list){
+
+    public static int [] waitingTime(List<? extends Process> list){
         int[] waitingTime =new int[list.size()];
         for (int i=1;i<list.size();i++){
             waitingTime[i]=list.get(i-1).getBurstTime()+waitingTime[i-1];
         }
         return waitingTime;
     }
-
-    public static int[] turnAroundTime(List<? extends  Process> list, int[] waitingTime){
+    public static int [] turnAroundTime(List<? extends  Process> list, int[] waitingTime){
         int[] turnAroundTime=new int[list.size()];
         for (int i=0;i<list.size();i++){
             turnAroundTime[i]=list.get(i).getBurstTime()+waitingTime[i];
         }
         return turnAroundTime;
     }
-    public static void printList(List<? extends Process> list, int[] waitingTime, int[] turnAroundTime){
+    public static void printList( List<? extends Process> list, int[] waitingTime, int[] turnAroundTime){
         System.out.println("Process\tBurst time\tWaiting Time\tTurn around time");
         for (int i=0;i<list.size();i++){
             System.out.println(list.get(i).getProcessId()+"\t\t\t"+list.get(i).getBurstTime()+"\t\t\t"+waitingTime[i]+"\t\t\t"+turnAroundTime[i]);
         }
-        float averageWaitingTime=AverageWaitingTime(waitingTime);
+        float averageWaitingTime=AverageTime(waitingTime);
         System.out.println("Average waiting time :"+averageWaitingTime);
-        float averageTurnAroundTime=AverageTurnAroundTime(turnAroundTime);
+        float averageTurnAroundTime=AverageTime(turnAroundTime);
         System.out.println("Average turn Around time :"+averageTurnAroundTime);
 
 
     }
-    public static float AverageWaitingTime(int[] waitingTime){
+    public static float AverageTime(int [] Time){
         float avg=0;
-        for (int i:waitingTime){
+        for (int i:Time){
             avg+=(float)i;
         }
-        return avg/=waitingTime.length;
+        return avg/=Time.length;
     }
-    public static float AverageTurnAroundTime(int[] turnAroundTime){
-        float avg=0;
-        for (int i:turnAroundTime){
-            avg+=(float)i;
-        }
-        return avg/=turnAroundTime.length;
-    }
+
 }
