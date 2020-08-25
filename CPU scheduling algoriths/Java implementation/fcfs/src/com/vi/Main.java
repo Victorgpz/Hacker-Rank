82ccdbf695853c0125f743012df3f8cc83431311
Victor <35284183+Victorgpz@users.noreply.github.com>
2020-08-25 23:35:15 +0530

    Update Main.java

--- CPU scheduling algoriths/Java implementation/fcfs/src/com/vi/Main.java
+++ CPU scheduling algoriths/Java implementation/fcfs/src/com/vi/Main.java
@@ -1,5 +1,5 @@
+
 package com.vi;
-import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,38 +17,38 @@ public class Main {
 
     }
 
-    public static int @NotNull [] waitingTime(List<? extends Process> list){
+    public static int [] waitingTime(List<? extends Process> list){
         int[] waitingTime =new int[list.size()];
         for (int i=1;i<list.size();i++){
             waitingTime[i]=list.get(i-1).getBurstTime()+waitingTime[i-1];
         }
         return waitingTime;
     }
-    public static int @NotNull [] turnAroundTime(List<? extends  Process> list, int[] waitingTime){
+    public static int [] turnAroundTime(List<? extends  Process> list, int[] waitingTime){
         int[] turnAroundTime=new int[list.size()];
         for (int i=0;i<list.size();i++){
             turnAroundTime[i]=list.get(i).getBurstTime()+waitingTime[i];
         }
         return turnAroundTime;
     }
-    public static void printList(@NotNull List<? extends Process> list, int[] waitingTime, int[] turnAroundTime){
+    public static void printList( List<? extends Process> list, int[] waitingTime, int[] turnAroundTime){
         System.out.println("Process\tBurst time\tWaiting Time\tTurn around time");
         for (int i=0;i<list.size();i++){
             System.out.println(list.get(i).getProcessId()+"\t\t\t"+list.get(i).getBurstTime()+"\t\t\t"+waitingTime[i]+"\t\t\t"+turnAroundTime[i]);
         }
-      float averageWaitingTime=AverageTime(waitingTime);
-      System.out.println("Average waiting time :"+averageWaitingTime);
-      float averageTurnAroundTime=AverageTime(turnAroundTime);
-      System.out.println("Average turn Around time :"+averageTurnAroundTime);
+        float averageWaitingTime=AverageTime(waitingTime);
+        System.out.println("Average waiting time :"+averageWaitingTime);
+        float averageTurnAroundTime=AverageTime(turnAroundTime);
+        System.out.println("Average turn Around time :"+averageTurnAroundTime);
 
 
     }
-    public static float AverageTime(int @NotNull [] Time){
+    public static float AverageTime(int [] Time){
         float avg=0;
         for (int i:Time){
             avg+=(float)i;
         }
-       return avg/=Time.length;
+        return avg/=Time.length;
     }
 
 }
