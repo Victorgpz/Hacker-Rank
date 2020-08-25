36d5095dff24eb7e4f4a5e8b228cc8788ca49c16
Victor <35284183+Victorgpz@users.noreply.github.com>
2020-08-26 00:12:56 +0530

    Add files via upload

---
+++ Java implementation/sjfs/src/com/vi/Process.java
@@ -0,0 +1,19 @@
+package com.vi;
+
+public class Process {
+    private String processId;
+    private int burstTime;
+
+    public Process(String processId, int burstTime) {
+        this.processId = processId;
+        this.burstTime = burstTime;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+}
