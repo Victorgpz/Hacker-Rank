c4156ce44b4564a4cb34a68b0ab89d467bccd655
vic <touchvictor@gmail.com>
2021-06-02 12:03:23 +0530

    clean up

--- java/Multiple-client-server/src/com/io/vic/Server.java
+++ java/Multiple-client-server/src/com/io/vic/Server.java
@@ -1,27 +1,21 @@
 package com.io.vic;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
     private static ServerSocket socket;
     private static Socket client;
-    private static PrintWriter out;
-    private static BufferedReader in;
-    private static ArrayList<ClientHandler> clientHandlers;
-    private static ExecutorService executorServicepool= Executors.newFixedThreadPool(5);
+
+  //  private static ArrayList<ClientHandler> clientHandlers;
+    private static ExecutorService executorServices = Executors.newFixedThreadPool(5);
 
     public static void main(String[] args) {
-        clientHandlers=new ArrayList<>();
+     //   clientHandlers=new ArrayList<>();
         {
             try {
                 socket = new ServerSocket(8088);
@@ -29,15 +23,14 @@ public class Server {
                     client = socket.accept();
                     System.out.println("Server connected to client");
                     ClientHandler clientHandler=new ClientHandler(client);
-                    clientHandlers.add(clientHandler);
+                  //  clientHandlers.add(clientHandler);
 
-                    executorServicepool.execute(clientHandler);
+                    executorServices.execute(clientHandler);
                 }
 
             } catch (IOException e) {
                 e.printStackTrace();
             }finally {
-                out.close();
                 try {
                     client.close();
                 } catch (IOException e) {
@@ -46,8 +39,4 @@ public class Server {
             }
         }
     }
-
-
-
-
 }
